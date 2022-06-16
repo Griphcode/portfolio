b4c2cf67e3777ceaa52547812fd45265a5172fe4
Griphcode <Griphcode@protonmail.com>
2022-06-16 23:38:15 +0200

    React app

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react'
+import { ReactNode } from 'react';
+import '../assets/App.css'
+
+
+export default function Main() {
+	return(
+		<Heading>Griphcode.</Heading>
+ );
+}
